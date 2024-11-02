262b4dda491de43f09ba52caef730417e19d2644
wiirijofct <y.hayduk@campus.fct.unl.pt>
2024-11-02 17:59:24 +0000

    updated

--- scc2425-tukano/src/main/java/tukano/impl/JavaShorts.java
+++ scc2425-tukano/src/main/java/tukano/impl/JavaShorts.java
@@ -29,31 +29,31 @@ import utils.RedisCache;
 public class JavaShorts implements Shorts {
 
 	private static Logger Log = Logger.getLogger(JavaShorts.class.getName());
-	
+
 	private static Shorts instance;
 
 	private static final int SHORT_CACHE_TTL = 5; // 5 seconds
-	
+
 	synchronized public static Shorts getInstance() {
-		if( instance == null )
+		if (instance == null)
 			instance = new JavaShorts();
 		return instance;
 	}
-	
-	private JavaShorts() {}
-	
-	
+
+	private JavaShorts() {
+	}
+
 	@Override
 	public Result<Short> createShort(String userId, String password) {
 		Log.info(() -> format("createShort : userId = %s, pwd = %s\n", userId, password));
 
-		return errorOrResult( okUser(userId, password), user -> {
+		return errorOrResult(okUser(userId, password), user -> {
 			var shortId = format("%s+%s", userId, UUID.randomUUID());
-			var blobUrl = format("%s/%s/%s", TukanoRestServer.serverURI, Blobs.NAME, shortId); 
+			var blobUrl = format("%s/%s/%s", TukanoRestServer.serverURI, Blobs.NAME, shortId);
 			var shrt = new Short(shortId, userId, blobUrl);
 
 			var insertResult = DB.insertOne(shrt);
-			if( !insertResult.isOK() ){
+			if (!insertResult.isOK()) {
 				return insertResult;
 			}
 
@@ -65,7 +65,7 @@ public class JavaShorts implements Shorts {
 				var value = JSON.encode(completeShort);
 				jedis.set(key, value);
 				jedis.expire(key, SHORT_CACHE_TTL);
-			
+
 			}
 			return ok(completeShort);
 		});
@@ -75,14 +75,14 @@ public class JavaShorts implements Shorts {
 	public Result<Short> getShort(String shortId) {
 		Log.info(() -> format("getShort : shortId = %s\n", shortId));
 
-		if( shortId == null )
+		if (shortId == null)
 			return error(BAD_REQUEST);
 
 		try (Jedis jedis = RedisCache.getCachePool().getResource()) {
-		
+
 			var key = "shorts:" + shortId;
 			var value = jedis.get(key);
-			if( value != null ) {
+			if (value != null) {
 				jedis.expire(key, SHORT_CACHE_TTL);
 				return Result.ok(JSON.decode(value, Short.class));
 			}
@@ -90,27 +90,39 @@ public class JavaShorts implements Shorts {
 
 		var query = format("SELECT count(*) FROM Likes l WHERE l.shortId = '%s'", shortId);
 		var likes = DB.sql(query, Long.class);
-		return errorOrValue( getOne(shortId, Short.class), shrt -> shrt.copyWithLikes_And_Token( likes.get(0)));
+		return errorOrValue(getOne(shortId, Short.class), shrt -> shrt.copyWithLikes_And_Token(likes.get(0)));
 	}
 
-	
 	@Override
 	public Result<Void> deleteShort(String shortId, String password) {
 		Log.info(() -> format("deleteShort : shortId = %s, pwd = %s\n", shortId, password));
-		
-		return errorOrResult( getShort(shortId), shrt -> {
-			
-			return errorOrResult( okUser( shrt.getOwnerId(), password), user -> {
-				return DB.transaction( hibernate -> {
 
-					//hibernate.remove( shrt);
-					
-					var query = format("DELETE Likes l WHERE l.shortId = '%s'", shortId);
-					//hibernate.createNativeQuery( query, Likes.class).executeUpdate();
-					
-					JavaBlobs.getInstance().delete(shrt.getBlobUrl(), Token.get() );
+		return errorOrResult(getShort(shortId), shrt -> {
+
+			return errorOrResult(okUser(shrt.getOwnerId(), password), user -> {
+				return DB.transaction(hibernate -> {
+
+					// hibernate.remove( shrt);
+
+					Result<Short> res = DB.deleteOne(shrt);
+					if (res.error() != null) {
+						return Result.error(res.error());
+					}
+					String likesQuery = format("SELECT * FROM c WHERE c.shortId = '%s' AND c.type = 'like'", shortId);
+					List<Short> likesList = DB.sql(likesQuery, Short.class);
+					// var query = format("DELETE Likes l WHERE l.shortId = '%s'", shortId);
+					// hibernate.createNativeQuery( query, Likes.class).executeUpdate();
+					for (Short like : likesList) {
+						Result<Short> deleteLikeRes = DB.deleteOne(like);
+						if (deleteLikeRes.error() != null) {
+							return Result.error(deleteLikeRes.error());
+						}
+					}
+
+					JavaBlobs.getInstance().delete(shrt.getBlobUrl(), Token.get());
+					return Result.ok();
 				});
-			});	
+			});
 		});
 	}
 
@@ -119,36 +131,36 @@ public class JavaShorts implements Shorts {
 		Log.info(() -> format("getShorts : userId = %s\n", userId));
 
 		var query = format("SELECT s.shortId FROM Short s WHERE s.ownerId = '%s'", userId);
-		return errorOrValue( okUser(userId), DB.sql( query, String.class));
+		return errorOrValue(okUser(userId), DB.sql(query, String.class));
 	}
 
 	@Override
 	public Result<Void> follow(String userId1, String userId2, boolean isFollowing, String password) {
-		Log.info(() -> format("follow : userId1 = %s, userId2 = %s, isFollowing = %s, pwd = %s\n", userId1, userId2, isFollowing, password));
-	
-		
-		return errorOrResult( okUser(userId1, password), user -> {
+		Log.info(() -> format("follow : userId1 = %s, userId2 = %s, isFollowing = %s, pwd = %s\n", userId1, userId2,
+				isFollowing, password));
+
+		return errorOrResult(okUser(userId1, password), user -> {
 			var f = new Following(userId1, userId2);
-			return errorOrVoid( okUser( userId2), isFollowing ? DB.insertOne( f ) : DB.deleteOne( f ));	
-		});			
+			return errorOrVoid(okUser(userId2), isFollowing ? DB.insertOne(f) : DB.deleteOne(f));
+		});
 	}
 
 	@Override
 	public Result<List<String>> followers(String userId, String password) {
 		Log.info(() -> format("followers : userId = %s, pwd = %s\n", userId, password));
 
-		var query = format("SELECT f.follower FROM Following f WHERE f.followee = '%s'", userId);		
-		return errorOrValue( okUser(userId, password), DB.sql(query, String.class));
+		var query = format("SELECT f.follower FROM Following f WHERE f.followee = '%s'", userId);
+		return errorOrValue(okUser(userId, password), DB.sql(query, String.class));
 	}
 
 	@Override
 	public Result<Void> like(String shortId, String userId, boolean isLiked, String password) {
-		Log.info(() -> format("like : shortId = %s, userId = %s, isLiked = %s, pwd = %s\n", shortId, userId, isLiked, password));
+		Log.info(() -> format("like : shortId = %s, userId = %s, isLiked = %s, pwd = %s\n", shortId, userId, isLiked,
+				password));
 
-		
-		return errorOrResult( getShort(shortId), shrt -> {
+		return errorOrResult(getShort(shortId), shrt -> {
 			var l = new Likes(userId, shortId, shrt.getOwnerId());
-			return errorOrVoid( okUser( userId, password), isLiked ? DB.insertOne( l ) : DB.deleteOne( l ));	
+			return errorOrVoid(okUser(userId, password), isLiked ? DB.insertOne(l) : DB.deleteOne(l));
 		});
 	}
 
@@ -156,11 +168,11 @@ public class JavaShorts implements Shorts {
 	public Result<List<String>> likes(String shortId, String password) {
 		Log.info(() -> format("likes : shortId = %s, pwd = %s\n", shortId, password));
 
-		return errorOrResult( getShort(shortId), shrt -> {
-			
-			var query = format("SELECT l.userId FROM Likes l WHERE l.shortId = '%s'", shortId);					
-			
-			return errorOrValue( okUser( shrt.getOwnerId(), password ), DB.sql(query, String.class));
+		return errorOrResult(getShort(shortId), shrt -> {
+
+			var query = format("SELECT l.userId FROM Likes l WHERE l.shortId = '%s'", shortId);
+
+			return errorOrValue(okUser(shrt.getOwnerId(), password), DB.sql(query, String.class));
 		});
 	}
 
@@ -169,50 +181,83 @@ public class JavaShorts implements Shorts {
 		Log.info(() -> format("getFeed : userId = %s, pwd = %s\n", userId, password));
 
 		final var QUERY_FMT = """
-				SELECT s.shortId, s.timestamp FROM Short s WHERE	s.ownerId = '%s'				
-				UNION			
-				SELECT s.shortId, s.timestamp FROM Short s, Following f 
-					WHERE 
-						f.followee = s.ownerId AND f.follower = '%s' 
+				SELECT s.shortId, s.timestamp FROM Short s WHERE	s.ownerId = '%s'
+				UNION
+				SELECT s.shortId, s.timestamp FROM Short s, Following f
+					WHERE
+						f.followee = s.ownerId AND f.follower = '%s'
 				ORDER BY s.timestamp DESC""";
 
-		return errorOrValue( okUser( userId, password), DB.sql( format(QUERY_FMT, userId, userId), String.class));		
+		return errorOrValue(okUser(userId, password), DB.sql(format(QUERY_FMT, userId, userId), String.class));
 	}
-		
-	protected Result<User> okUser( String userId, String pwd) {
+
+	protected Result<User> okUser(String userId, String pwd) {
 		return JavaUsers.getInstance().getUser(userId, pwd);
 	}
-	
-	private Result<Void> okUser( String userId ) {
-		var res = okUser( userId, "");
-		if( res.error() == FORBIDDEN )
+
+	private Result<Void> okUser(String userId) {
+		var res = okUser(userId, "");
+		if (res.error() == FORBIDDEN)
 			return ok();
 		else
-			return error( res.error() );
+			return error(res.error());
 	}
-	
+
 	@Override
 	public Result<Void> deleteAllShorts(String userId, String password, String token) {
 		Log.info(() -> format("deleteAllShorts : userId = %s, password = %s, token = %s\n", userId, password, token));
 
-		if( ! Token.isValid( token, userId ) )
+		if (!Token.isValid(token, userId))
 			return error(FORBIDDEN);
-		
-		return DB.transaction( (hibernate) -> {
-						
-			//delete shorts
-			var query1 = format("DELETE Short s WHERE s.ownerId = '%s'", userId);		
-			//hibernate.createQuery(query1, Short.class).executeUpdate();
-			
-			//delete follows
-			var query2 = format("DELETE Following f WHERE f.follower = '%s' OR f.followee = '%s'", userId, userId);		
-			//hibernate.createQuery(query2, Following.class).executeUpdate();
-			
-			//delete likes
-			var query3 = format("DELETE Likes l WHERE l.ownerId = '%s' OR l.userId = '%s'", userId, userId);		
-			//hibernate.createQuery(query3, Likes.class).executeUpdate();
-			
+
+		return DB.transaction((hibernate) -> {
+
+			// //delete shorts
+			// var query1 = format("DELETE Short s WHERE s.ownerId = '%s'", userId);
+			// //hibernate.createQuery(query1, Short.class).executeUpdate();
+
+			// //delete follows
+			// var query2 = format("DELETE Following f WHERE f.follower = '%s' OR f.followee
+			// = '%s'", userId, userId);
+			// //hibernate.createQuery(query2, Following.class).executeUpdate();
+
+			// //delete likes
+			// var query3 = format("DELETE Likes l WHERE l.ownerId = '%s' OR l.userId =
+			// '%s'", userId, userId);
+			// //hibernate.createQuery(query3, Likes.class).executeUpdate();
+
+			// delete shorts
+			String deleteShortsQuery = format("SELECT * FROM c WHERE c.ownerId = '%s'", userId);
+			List<Short> shortsList = DB.sql(deleteShortsQuery, Short.class);
+			for (Short shortObj : shortsList) {
+				Result<Short> deleteShortRes = DB.deleteOne(shortObj);
+				if (deleteShortRes.error() != null) {
+					return Result.error(deleteShortRes.error());
+				}
+			}
+			// delete follows
+			String deleteFollowsQuery = format(
+					"SELECT * FROM c WHERE c.follower = '%s' OR c.followee = '%s' AND c.type ='following'", userId,
+					userId);
+			List<Short> followsList = DB.sql(deleteFollowsQuery, Short.class);
+			for (Short follow : followsList) {
+				Result<Short> deleteFollowRes = DB.deleteOne(follow);
+				if (deleteFollowRes.error() != null) {
+					return Result.error(deleteFollowRes.error());
+				}
+			}
+			// delete likes
+			String deleteLikesQuery = format("SELECT * FROM c WHERE c.userId = '%s' AND c.type = 'like'", userId);
+			List<Short> likesList = DB.sql(deleteLikesQuery, Short.class);
+			for (Short like : likesList) {
+				Result<Short> deleteLikeRes = DB.deleteOne(like);
+				if (deleteLikeRes.error() != null) {
+					return Result.error(deleteLikeRes.error());
+				}
+			}
+			return Result.ok();
+
 		});
 	}
-	
+
 }
